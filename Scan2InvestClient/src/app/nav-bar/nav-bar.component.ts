ff340472033fa6c76b0bc545ff29d03339a672db
AaronJAdler <aaronadlerjaden@gmail.com>
2023-10-13 15:33:18 -0700

    fix

---
+++ Scan2InvestClient/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,81 @@
+import { Component } from '@angular/core';
+import { FileUploadService } from '../image-upload/image-upload.service';
+import { HttpEventType, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent {
+  selectedFiles?: FileList;
+  currentFile?: File;
+  progress = 0;
+  message = '';
+  preview = '';
+  imageInfos?: Observable<any>;
+
+  constructor(private uploadService: FileUploadService) { }
+
+
+  selectFile(event: any): void {
+    this.message = '';
+    this.progress = 0;
+    this.selectedFiles = event.target.files;
+
+    if (this.selectedFiles) {
+      const file: File | null = this.selectedFiles.item(0);
+
+      if (file) {
+        this.currentFile = file;
+
+        const reader = new FileReader();
+
+        reader.onload = (e: any) => {
+          console.log(e.target.result);
+        };
+
+        reader.readAsDataURL(this.currentFile);
+        this.upload();
+      }
+    }
+  }
+
+  upload(): void {
+    this.progress = 0;
+
+    if (this.selectedFiles) {
+      const file: File | null = this.selectedFiles.item(0);
+
+      if (file) {
+        this.currentFile = file;
+
+        this.uploadService.upload(this.currentFile).subscribe({
+          next: (event: any) => {
+            if (event.type === HttpEventType.UploadProgress) {
+              this.progress = Math.round((100 * event.loaded) / event.total);
+            } else if (event instanceof HttpResponse) {
+              this.message = event.body.message;
+              this.imageInfos = this.uploadService.getFiles();
+            }
+          },
+          error: (err: any) => {
+            console.log(err);
+            this.progress = 0;
+
+            if (err.error && err.error.message) {
+              this.message = err.error.message;
+            } else {
+              this.message = 'Could not upload the image!';
+            }
+
+            this.currentFile = undefined;
+          },
+        });
+      }
+
+      this.selectedFiles = undefined;
+    }
+  }
+}
